0d60f0b25bf459b3e05749ca49a97f23f687afc8
rodriguezhr25 <rodriguezhr25@gmail.com>
2020-01-12 02:14:08 -0400

    First commit of project

---
+++ js/functions.js
@@ -0,0 +1,9 @@
+
+
+    var today = new Date();
+    var actualYear = today.getFullYear();
+    document.getElementById("year").innerHTML = actualYear;
+    
+    document.getElementById('dateUpdated').textContent=document.lastModified;
+
+
