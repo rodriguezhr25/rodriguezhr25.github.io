88a5bb0e98f1ca816329effa6d950cbbf924e04e
rodriguezhr25 <rodriguezhr25@gmail.com>
2020-01-12 02:45:53 -0400

    Final details

--- js/functions.js
+++ js/functions.js
@@ -2,4 +2,4 @@ var today = new Date();
 var actualYear = today.getFullYear();
 document.getElementById("year").innerHTML = actualYear;
 
-document.getElementById('dateUpdated').textContent = document.lastModified;
+document.getElementById('dateUpdated').textContent = "Last Updated:" + document.lastModified;
