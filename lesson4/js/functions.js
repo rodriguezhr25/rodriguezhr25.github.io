e47d23bbc8e4848478ab7a6568ddb75ca88fc8ac
rodriguezhr25 <rodriguezhr25@gmail.com>
2020-02-01 23:53:33 -0400

    lesson4

---
+++ lesson4/js/functions.js
@@ -0,0 +1,13 @@
+var today = new Date();
+var days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+var months = ['January','February','March','April','May','June','July','August','September','October','November','December'];
+
+
+document.getElementById('dateUpdated').textContent = days[today.getDay()] + ", "  + " " +today.getDate() +" " + months[today.getMonth()]+ " " + today.getFullYear();
+
+const hambutton = document.querySelector(".ham");
+hambutton.addEventListener("click", toggleMenu, false);
+
+function toggleMenu() {
+  document.querySelector(".navigation").classList.toggle("responsive");
+}
