33a06dcc0a4edf167592adffaa54429b30da3c6c
rodriguezhr25 <rodriguezhr25@gmail.com>
2020-02-11 13:14:24 -0400

    Documents format

--- lesson5/js/functions.js
+++ lesson5/js/functions.js
@@ -1,22 +1,20 @@
 var today = new Date();
-var days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
-var months = ['January','February','March','April','May','June','July','August','September','October','November','December'];
+var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
 
-document.getElementById('dateUpdated').textContent = days[today.getDay()] + ", "  + " " +today.getDate() +" " + months[today.getMonth()]+ " " + today.getFullYear();
+document.getElementById('dateUpdated').textContent = days[today.getDay()] + ", " + " " + today.getDate() + " " + months[today.getMonth()] + " " + today.getFullYear();
 
 const hambutton = document.querySelector(".ham");
 hambutton.addEventListener("click", toggleMenu, false);
 
 function toggleMenu() {
-  document.querySelector(".navigation").classList.toggle("responsive");
+    document.querySelector(".navigation").classList.toggle("responsive");
 }
 
 var day = new Date();
 var today = day.getDay();
 
-if(today == 5) {
-	document.getElementById("message").className = "show";
+if (today == 5) {
+    document.getElementById("message").className = "show";
 }
-
-
