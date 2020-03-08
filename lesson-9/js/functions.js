b25551bfffb973b1edc9f7ca58dbaa99bce0f4a0
rodriguezhr25 <rodriguezhr25@gmail.com>
2020-03-07 20:22:28 -0400

    update

---
+++ lesson-9/js/functions.js
@@ -0,0 +1,6 @@
+var today = new Date();
+var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+
+//document.getElementsByTagNameNS('dateUpdated').textContent = days[today.getDay()] + ", " + " " + today.getDate() + " " + months[today.getMonth()] + " " + today.getFullYear();
